2014fccc3f9365dc31f886b73e78b73a419b845d
Vinnie Lauro <devops-github@mail.vinniehat.com>
2025-05-06 16:25:38 -0400

    Update lockbit_background.ps1

--- lockbit_background.ps1
+++ lockbit_background.ps1
@@ -1,25 +1,19 @@
-$imageUrl = 'https://cyble.com/wp-content/uploads/2021/08/Cyble-LOCKBIT-Ransomware-Windows-darkweb-LOCKBIT-2.0-Changing-Desktop-Background.png'
-$username = $([System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split('\')[1])
-$imagePath = "C:\Users\$username\Pictures\downloaded_image.jpg"
+$u = $env:USERNAME
+$p = "C:\Users\$u\Pictures\dl.jpg"
 
-Invoke-WebRequest -Uri $imageUrl -OutFile $imagePath
+Invoke-WebRequest -Uri "https://cyble.com/wp-content/uploads/2021/08/Cyble-LOCKBIT-Ransomware-Windows-darkweb-LOCKBIT-2.0-Changing-Desktop-Background.png" -OutFile $p
 
-# Set wallpaper mode to "Picture" (disable slideshow)
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers' -Name BackgroundType -Value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" -Name BackgroundType -Value 1
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\" -Name Wallpaper -Value $p
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\" -Name WallpaperStyle -Value 2
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\" -Name TileWallpaper -Value 0
 
-# Set the image as wallpaper
-Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name 'Wallpaper' -Value $imagePath
-Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name 'WallpaperStyle' -Value 2
-Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name 'TileWallpaper' -Value 0
-
-# Force the update
 Add-Type -TypeDefinition @"
-    using System;
-    using System.Runtime.InteropServices;
-    public class Wallpaper {
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-    }
+using System;
+using System.Runtime.InteropServices;
+public class W {
+    [DllImport("user32.dll")]
+    public static extern int SystemParametersInfo(int a, int b, string c, int d);
+}
 "@
-
-[Wallpaper]::SystemParametersInfo(20, 0, $imagePath, 0x01 | 0x02)
+[W]::SystemParametersInfo(20, 0, $p, (0x01 -bor 0x02))
